543a520300fa3f2f06471583cf8fcb9ead64772e
larssonbajra <larssonbajra@gmail.com>
2019-02-12 22:48:29 +0100

    Created services

---
+++ OurForum.Data/Interface/IForum.cs
@@ -0,0 +1,18 @@
+﻿using OurForum.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OurForum.Data.Interface
+{
+	public interface IForum
+	{
+		Forum GetById(int id);
+		IEnumerable<Forum> GetAll();
+		IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+		Task Create(Forum forum); // task similar to void, but represents async event
+		Task Delete(int forumId);
+		Task UpdateForumTitle(int forumId, string newTitle);
+		Task UpdateForumDescription(int forumId, string newDescription);
+	}
+}
---
+++ OurForum.Data/Migrations/20190212210001_Initial models added again.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace OurForum.Data.Migrations
+{
+    public partial class Initialmodelsaddedagain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
